61ee5390fa38a45e7fa026b63071f06c33873803
Mirikz <kirillmitrofanov68@gmail.com>
2022-01-19 10:05:05 +0500

    m

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿double devise(double a, double b) { 
-return a / b;
-}
-
+﻿Console.WriteLine("Введите а");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b");
+double b = double.Parse(Console.ReadLine()); 
+Console.WriteLine(plus(a, b)));
+Console.WriteLine(minus(a, b));
+Console.WriteLine(multi(a, b));
+Console.WriteLine(devise(a, b));
